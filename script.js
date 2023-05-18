04159bac156347eb74fdab476a662251277cbdd0
josemino <83043558+josemino@users.noreply.github.com>
2023-05-18 11:38:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-  // Obtener la URL actual
-  var currentLocation = window.location.href;
-  var menuItems = document.querySelectorAll("nav ul li a");
 
-  // Recorrer los elementos del menú
-  for (var i = 0; i < menuItems.length; i++) {
-    // Verificar si la URL del enlace coincide con la URL actual
-    if (menuItems[i].href === currentLocation) {
-      // Agregar la clase "active" al elemento correspondiente
-      menuItems[i].classList.add("active");
-    }
-  }
