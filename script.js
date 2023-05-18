322eed442f32d97b9575c57f868b1a5e4ee0e937
josemino <83043558+josemino@users.noreply.github.com>
2023-05-18 09:10:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Obtener la URL de la página actual
+var url = window.location.href;
 
+// Obtener el nombre del archivo de la URL
+var filename = url.substring(url.lastIndexOf('/') + 1);
+
+// Obtener el nombre del archivo sin la extensión
+var page = filename.split('.')[0];
+
+// Agregar la clase "active" al elemento del menú correspondiente
+document.getElementById(page).classList.add('active');
